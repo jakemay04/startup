e6223b3bbaf578a4ac054760bbbb901dcc2b8124
Jake May <pmjdmay@gmail.com>
2025-11-05 00:42:56 -0700

    impliment login backend

--- service/index.js
+++ service/index.js
@@ -1,2 +1,63 @@
+const express = require('express');
+const app = express();
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.static('public'));
+
+app.use(express.json());
+
+const uuid = require('uuid');
+
+token: uuid.v4(); // make new token
+
+// registration
+app.post('/api/auth', async (req, res) => {
+  if (await getUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+    res.send({ email: user.email });
+  }
+});
+
+// login
+app.put('/api/auth', async (req, res) => {
+  res.send({ email: 'marta@id.com' });
+});
+
+// logout
+app.delete('/api/auth', async (req, res) => {
+  res.send({});
+});
+
+// getMe
+app.get('/api/user', async (req, res) => {
+  res.send({ email: 'marta@id.com' });
+});
+
+app.listen(4000);
+
+
+const bcrypt = require('bcryptjs');
+
+const users = [];
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+  };
+
+  users.push(user);
+
+  return user;
+}
+
+function getUser(field, value) {
+  if (value) {
+    return users.find((user) => user[field] === value);
+  }
+  return null;
+}
