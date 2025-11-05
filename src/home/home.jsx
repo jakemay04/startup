5cbddfccf905183b7db782d9eeffd4bec8b1075c
Jake May <pmjdmay@gmail.com>
2025-11-05 11:48:22 -0700

    home functionality works

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -7,7 +7,7 @@ const PostCard = ({ post }) => {
     <div className="post-card">
       <div className="profile-info">
         <div className="profile-pic">{post.email?.[0] || 'U'}</div>
-        <div className="mock_user">{post.email || 'User'}</div>
+        <div className="mock_user">{post.username || 'User'}</div>
       </div>
       <p>{post.content}</p>
     </div>
@@ -28,7 +28,7 @@ const PostInput = ({ onPostSubmit }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (postContent.trim()) {
-      onPostSubmit({ id: Date.now(), content: postContent, name: user?.username || user?.email});
+      onPostSubmit({ id: Date.now(), content: postContent, username: user?.username || user?.email, email: user?.email });
       setPostContent('');
     }
   };
@@ -57,7 +57,7 @@ export function Home() {
   }
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>Logged in as: {user?.email || 'Guest'}</div>
+      <div>Logged in as: {user?.username || user?.email}</div>
       <div className="feed-container">
         <div className="feed-item-profile">
           <PostInput onPostSubmit={handlePostSubmit} />
